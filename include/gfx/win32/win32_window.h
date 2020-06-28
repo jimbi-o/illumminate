c6b68e3d958ddf9554fae06b83d1071e9a630900
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2020-06-28 10:34:45 +0900

    logger added

--- include/gfx/win32/win32_window.h
+++ include/gfx/win32/win32_window.h
@@ -1,3 +1,5 @@
 #ifndef __ILLUMINATE_WIN32_H__
 #define __ILLUMINATE_WIN32_H__
+namespace illuminate::gfx::win32 {
+}
 #endif
--- src/gfx/win32/win32_window.cpp
+++ src/gfx/win32/win32_window.cpp
@@ -1,5 +1,7 @@
 #include "win32_window.h"
 #include "doctest/doctest.h"
+#include "logger.h"
 TEST_CASE("test") {
+  loginfo("hello world");
   CHECK(true);
 }
