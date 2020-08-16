c7e8d3f7b147f14e7a5eb43f8558f4a6d38b7a78
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2020-08-16 09:11:04 +0900

    command list type considered for command list index

--- src/gfx/d3d12/d3d12_command_executor.cpp
+++ src/gfx/d3d12/d3d12_command_executor.cpp
@@ -25,9 +25,9 @@ struct RenderGraphConfig {
   std::vector<BatchedRenderPassConfig> batch;
 };
 struct ParsedRenderGraphPassInfo {
-  uint32_t command_list_index_prev_function;
-  uint32_t command_list_index_render_function;
-  uint32_t command_list_index_post_function;
+  std::unordered_map<CommandListType, uint32_t> command_list_index_prev_function;
+  std::unordered_map<CommandListType, uint32_t> command_list_index_render_function;
+  std::unordered_map<CommandListType, uint32_t> command_list_index_post_function;
   RenderPassFunction render_function;
   RenderPassResourceConfig resource_config;
 };
@@ -45,9 +45,9 @@ ParsedRenderGraphInfo ParseRenderGraph(RenderGraphConfig&& graph_config) {
     dst_batch.command_list_num[CommandListType::kGraphics] = 1; // TODO consider queue types used, parallel exec
     for (auto& src_pass : src_batch.pass) {
       ParsedRenderGraphPassInfo dst_pass{};
-      dst_pass.command_list_index_prev_function = 0; // TODO
-      dst_pass.command_list_index_render_function = 0; // TODO
-      dst_pass.command_list_index_post_function = 0; // TODO
+      dst_pass.command_list_index_prev_function[CommandListType::kGraphics] = 0; // TODO
+      dst_pass.command_list_index_render_function[CommandListType::kGraphics] = 0; // TODO
+      dst_pass.command_list_index_post_function[CommandListType::kGraphics] = 0; // TODO
       dst_pass.render_function = std::move(src_pass.render_function);
       dst_pass.resource_config.rtv = std::move(src_pass.resource_config.rtv);
       dst_batch.pass.push_back(std::move(dst_pass));
@@ -78,7 +78,6 @@ TEST_CASE("execute command list") {
   std::queue<std::vector<ID3D12CommandAllocator**>> allocators;
   for (uint32_t i = 0; i < 2 * buffer_num; i++) {
     CAPTURE(i);
-    swapchain.UpdateBackBufferIndex();
     if (i >= buffer_num) {
       command_queue.WaitOnCpu({{CommandListType::kGraphics, i - buffer_num}});
       for (auto a : allocators.back()) {
@@ -87,6 +86,7 @@ TEST_CASE("execute command list") {
       allocators.pop();
     }
     allocators.push({});
+    swapchain.UpdateBackBufferIndex();
     BatchedRenderPassConfig config {
       {
         {
@@ -115,11 +115,11 @@ TEST_CASE("execute command list") {
         barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
         barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
         barrier.Transition.StateAfter  = D3D12_RESOURCE_STATE_RENDER_TARGET;
-        command_lists[pass.command_list_index_prev_function]->ResourceBarrier(1, &barrier);
-        pass.render_function(&command_lists[pass.command_list_index_render_function], pass.resource_config);
+        command_lists[pass.command_list_index_prev_function[CommandListType::kGraphics]]->ResourceBarrier(1, &barrier);
+        pass.render_function(&command_lists[pass.command_list_index_render_function[CommandListType::kGraphics]], pass.resource_config);
         barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
         barrier.Transition.StateAfter  = D3D12_RESOURCE_STATE_PRESENT;
-        command_lists[pass.command_list_index_post_function]->ResourceBarrier(1, &barrier);
+        command_lists[pass.command_list_index_post_function[CommandListType::kGraphics]]->ResourceBarrier(1, &barrier);
       }
       for (uint32_t j = 0; j < batch.command_list_num[CommandListType::kGraphics]; j++) {
         auto hr = command_lists[j]->Close();
