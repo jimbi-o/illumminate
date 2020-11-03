6410c58d4f4ed5ba643bc4e0b46e2df4bacb6b18
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2020-11-03 20:57:34 +0900

    plain LinearAllocator added

--- src/core/memory.cpp
+++ src/core/memory.cpp
@@ -4,28 +4,43 @@ constexpr inline std::uintptr_t AlignAddress(const std::uintptr_t addr, const si
   const auto mask = align - 1;
   return (addr + mask) & ~mask;
 }
-class LinearAllocator : public std::pmr::memory_resource {
+class LinearAllocator {
  public:
   explicit LinearAllocator(const std::byte* buffer, const uint32_t size_in_byte) : head_(reinterpret_cast<uintptr_t>(buffer)), size_in_byte_(size_in_byte), offset_in_byte_(0) {}
   LinearAllocator() : head_(0), size_in_byte_(0), offset_in_byte_(0) {}
-  ~LinearAllocator() override {}
+  ~LinearAllocator() {}
   LinearAllocator(const LinearAllocator&) = delete;
   LinearAllocator& operator=(const LinearAllocator&) = delete;
   constexpr void Reset() { offset_in_byte_ = 0; }
- private:
-  inline void* do_allocate(std::size_t bytes, std::size_t alignment_in_bytes) override {
+  inline void* Allocate(std::size_t bytes, std::size_t alignment_in_bytes) {
     auto addr_aligned = AlignAddress(head_ + offset_in_byte_, alignment_in_bytes);
     offset_in_byte_ = addr_aligned - head_ + bytes;
     if (offset_in_byte_ > size_in_byte_) return nullptr;
     return reinterpret_cast<void*>(addr_aligned);
   }
-  constexpr inline void do_deallocate([[maybe_unused]] void* p, [[maybe_unused]] std::size_t bytes, [[maybe_unused]] std::size_t alignment) override {}
-  bool do_is_equal(const memory_resource& other) const noexcept override;
+  constexpr auto GetOffset() const { return offset_in_byte_; }
+ private:
   const std::uintptr_t head_;
   const size_t size_in_byte_;
   size_t offset_in_byte_;
 };
-bool LinearAllocator::do_is_equal(const memory_resource& other) const noexcept {
+class PmrLinearAllocator : public std::pmr::memory_resource {
+ public:
+  explicit PmrLinearAllocator(const std::byte* buffer, const uint32_t size_in_byte) : allocator_(buffer, size_in_byte) {}
+  PmrLinearAllocator() {}
+  ~PmrLinearAllocator() override {}
+  PmrLinearAllocator(const PmrLinearAllocator&) = delete;
+  PmrLinearAllocator& operator=(const PmrLinearAllocator&) = delete;
+  constexpr void Reset() { allocator_.Reset(); }
+ private:
+  inline void* do_allocate(std::size_t bytes, std::size_t alignment_in_bytes) override {
+    return allocator_.Allocate(bytes, alignment_in_bytes);
+  }
+  constexpr inline void do_deallocate([[maybe_unused]] void* p, [[maybe_unused]] std::size_t bytes, [[maybe_unused]] std::size_t alignment) override {}
+  bool do_is_equal(const memory_resource& other) const noexcept override;
+  LinearAllocator allocator_;
+};
+bool PmrLinearAllocator::do_is_equal(const memory_resource& other) const noexcept {
   return this == &other;
 }
 }
@@ -37,7 +52,7 @@ TEST_CASE("memory") {
   using namespace illuminate::core;
   const uint32_t size_in_byte = 1024;
   std::byte buffer[size_in_byte]{};
-  auto memory_resource = std::make_shared<LinearAllocator>(buffer, size_in_byte);
+  auto memory_resource = std::make_shared<PmrLinearAllocator>(buffer, size_in_byte);
   std::pmr::vector<uint32_t> v(memory_resource.get());
   v.reserve(3);
   v.push_back(0);
