8872870e96f02cd1cd3c1d4addb8974fba523e09
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2020-11-03 18:23:42 +0900

    custom memory allocator for std::pmr added

---
+++ src/core/memory.cpp
@@ -0,0 +1,91 @@
+#include <memory_resource>
+namespace illuminate::core {
+constexpr inline std::uintptr_t AlignAddress(const std::uintptr_t addr, const size_t align/*power of 2*/) {
+  const auto mask = align - 1;
+  return (addr + mask) & ~mask;
+}
+class LinearAllocator : public std::pmr::memory_resource {
+ public:
+  explicit LinearAllocator(const std::byte* buffer, const uint32_t size_in_byte) : head_(reinterpret_cast<uintptr_t>(buffer)), size_in_byte_(size_in_byte), offset_in_byte_(0) {}
+  LinearAllocator() : head_(0), size_in_byte_(0), offset_in_byte_(0) {}
+  ~LinearAllocator() override {}
+  LinearAllocator(const LinearAllocator&) = delete;
+  LinearAllocator& operator=(const LinearAllocator&) = delete;
+  constexpr void Reset() { offset_in_byte_ = 0; }
+ private:
+  inline void* do_allocate(std::size_t bytes, std::size_t alignment_in_bytes) override {
+    auto addr_aligned = AlignAddress(head_ + offset_in_byte_, alignment_in_bytes);
+    offset_in_byte_ = addr_aligned - head_ + bytes;
+    if (offset_in_byte_ > size_in_byte_) return nullptr;
+    return reinterpret_cast<void*>(addr_aligned);
+  }
+  constexpr inline void do_deallocate([[maybe_unused]] void* p, [[maybe_unused]] std::size_t bytes, [[maybe_unused]] std::size_t alignment) override {}
+  bool do_is_equal(const memory_resource& other) const noexcept override;
+  const std::uintptr_t head_;
+  const size_t size_in_byte_;
+  size_t offset_in_byte_;
+};
+bool LinearAllocator::do_is_equal(const memory_resource& other) const noexcept {
+  return this == &other;
+}
+}
+#include <memory>
+#include <vector>
+#include "doctest/doctest.h"
+TEST_CASE("memory") {
+  using namespace illuminate;
+  using namespace illuminate::core;
+  const uint32_t size_in_byte = 1024;
+  std::byte buffer[size_in_byte]{};
+  auto memory_resource = std::make_shared<LinearAllocator>(buffer, size_in_byte);
+  std::pmr::vector<uint32_t> v(memory_resource.get());
+  v.reserve(3);
+  v.push_back(0);
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  CHECK(v[0] == 0);
+  CHECK(v[1] == 1);
+  CHECK(v[2] == 2);
+  CHECK(v[3] == 3);
+  memory_resource->Reset();
+  CHECK(memory_resource->allocate(size_in_byte, 4) == buffer);
+  CHECK(memory_resource->allocate(1, 4) == nullptr);
+  memory_resource->Reset();
+  CHECK(memory_resource->allocate(size_in_byte, 4) == buffer);
+  memory_resource->Reset();
+  CHECK(memory_resource->allocate(500, 4) == buffer);
+  v.clear();
+  v.push_back(0);
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  CHECK(v[0] == 0);
+  CHECK(v[1] == 1);
+  CHECK(v[2] == 2);
+  CHECK(v[3] == 3);
+  std::pmr::vector<uint32_t> v2(memory_resource.get());
+  v2.push_back(10);
+  v2.push_back(11);
+  v2.push_back(12);
+  v2.push_back(13);
+  CHECK(v2[0] == 10);
+  CHECK(v2[1] == 11);
+  CHECK(v2[2] == 12);
+  CHECK(v2[3] == 13);
+  CHECK(v[0] == 0);
+  CHECK(v[1] == 1);
+  CHECK(v[2] == 2);
+  CHECK(v[3] == 3);
+  v.reserve(100);
+  v.push_back(101);
+  CHECK(v2[0] == 10);
+  CHECK(v2[1] == 11);
+  CHECK(v2[2] == 12);
+  CHECK(v2[3] == 13);
+  CHECK(v[0] == 0);
+  CHECK(v[1] == 1);
+  CHECK(v[2] == 2);
+  CHECK(v[3] == 3);
+  CHECK(v[4] == 101);
+}
