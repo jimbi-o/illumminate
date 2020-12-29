79e65e9d60cf71763ac26b956779190bcc110662
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2020-12-29 11:20:34 +0900

    d3d12 test init/term process added

--- src/gfx/d3d12/d3d12_test.cpp
+++ src/gfx/d3d12/d3d12_test.cpp
@@ -8,6 +8,25 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 TEST_CASE("d3d12/render") {
+  const uint32_t buffer_num = 2;
+  const uint32_t swapchain_buffer_num = buffer_num + 1;
+  using namespace illuminate::gfx;
+  using namespace illuminate::gfx::d3d12;
+  DxgiCore dxgi_core;
+  CHECK(dxgi_core.Init());
+  Device device;
+  CHECK(device.Init(dxgi_core.GetAdapter()));
+  CommandQueue command_queue;
+  CHECK(command_queue.Init(device.Get()));
+  illuminate::gfx::win32::Window window;
+  CHECK(window.Init("swapchain test", 160, 90));
+  Swapchain swapchain;
+  CHECK(swapchain.Init(dxgi_core.GetFactory(), command_queue.Get(CommandQueueType::kGraphics), device.Get(), window.GetHwnd(), swapchain_buffer_num, buffer_num));
+  CommandAllocator command_allocator;
+  CHECK(command_allocator.Init(device.Get()));
+  CommandList command_list;
+  CHECK(command_list.Init(device.Get()));
+  std::vector<std::vector<ID3D12CommandAllocator**>> allocators(buffer_num);
   SUBCASE("clear swapchain rtv@graphics queue") {
     // TODO
   }
@@ -23,4 +42,18 @@ TEST_CASE("d3d12/render") {
   SUBCASE("clear + draw moving triangle to swapchain uav") {
     // TODO
   }
+  while (!allocators.empty()) {
+    for (auto a : allocators.back()) {
+      command_allocator.ReturnCommandAllocator(a);
+    }
+    allocators.pop_back();
+  }
+  command_queue.WaitAll();
+  command_list.Term();
+  command_allocator.Term();
+  swapchain.Term();
+  window.Term();
+  command_queue.Term();
+  device.Term();
+  dxgi_core.Term();
 }
