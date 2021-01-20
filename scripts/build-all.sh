ee6d461da0f6026b508b848fddd9cc92e973691a
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2021-01-20 22:43:04 +0900

    dxcompiler added to project (cannot be build with cmake on windows w/ninja)

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -6,5 +6,5 @@ bash scripts/build.sh make windows core
 bash scripts/build.sh make windows gfx
 bash scripts/build.sh make windows gfx/d3d12
 bash scripts/build.sh make windows math
-# cmake.exe -A x64 -S src/gfx/d3d12 -B build/vs/gfx/d3d12 -T ClangCl
-# cmake.exe --build build/vs/gfx/d3d12 --config Debug && ./build/vs/gfx/d3d12/Debug/illuminategfxd3d12.exe
+# cmake.exe -A x64 -S src/gfx/d3d12 -B build/vs/gfx/d3d12; cmake.exe --build build/vs/gfx/d3d12 --config Debug
+# deprecated: cmake.exe -A x64 -S src/gfx/d3d12 -B build/vs/gfx/d3d12 -T ClangCl # ClangCl did not work w/dxc
