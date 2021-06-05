c056abd182b2e7cc4d20dd9195905b1c94ac2a2e
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2021-06-05 16:24:16 +0900

    clang-11 -> clang-12

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -6,5 +6,5 @@ bash scripts/build.sh make windows core
 bash scripts/build.sh make windows gfx
 bash scripts/build.sh make windows gfx/d3d12
 bash scripts/build.sh make windows math
-# cmake.exe -A x64 -S src/gfx/d3d12 -B build/vs/gfx/d3d12; cmake.exe --build build/vs/gfx/d3d12 --config Debug
 # deprecated: cmake.exe -A x64 -S src/gfx/d3d12 -B build/vs/gfx/d3d12 -T ClangCl # ClangCl did not work w/dxc
+# cmake.exe -S src/gfx -B build/vs/gfx -A x64 -DCMAKE_CXX_FLAGS="-Wall" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake.exe --build build/vs/gfx/d3d12 --config Debug
